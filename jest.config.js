feee5ab3033e94036c2036e1fb56bf04abe7e44b
Ammar Multani <ammarmultani007@gmail.com>
2025-01-13 18:15:03 +0530

    half jest setup

--- jest.config.js
+++ jest.config.js
@@ -1,44 +1,14 @@
-// jest.config.js
 module.exports = {
   preset: 'react-native',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  setupFiles: ['./jest.setup.js'],
   transformIgnorePatterns: [
-    'node_modules/(?!(@react-native|react-native|@react-navigation|@react-native-google-signin|react-native-fbsdk-next)/)',
+    'node_modules/(?!((@react-native|react-native|@react-navigation|@react-native-google-signin|react-native-fbsdk-next|react-native-vector-icons|react-native-gesture-handler|react-native-reanimated|@react-native-firebase)/.*))',
   ],
+  setupFilesAfterEnv: ['./setup.js'],
   moduleNameMapper: {
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/__mocks__/fileMock.js',
-  }
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
+  },
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
+  },
 };
-
-// jest.setup.js
-jest.mock('@react-native-google-signin/google-signin', () => ({
-  GoogleSignin: {
-    configure: jest.fn(),
-    signIn: jest.fn(),
-    signOut: jest.fn(),
-    isSignedIn: jest.fn(),
-    getCurrentUser: jest.fn(),
-  },
-  GoogleSigninButton: 'View',
-}));
-
-jest.mock('react-native-fbsdk-next', () => ({
-  LoginManager: {
-    logInWithPermissions: jest.fn(),
-    logOut: jest.fn(),
-  },
-  AccessToken: {
-    getCurrentAccessToken: jest.fn(),
-  },
-}));
-
-jest.mock('@react-native-async-storage/async-storage', () => ({
-  setItem: jest.fn(),
-  getItem: jest.fn(),
-  removeItem: jest.fn(),
-}));
-
-// __mocks__/fileMock.js
-module.exports = 'test-file-stub';
