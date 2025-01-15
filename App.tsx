00a7639f55fe1eaa3abece7bc4541e9b3853d195
Ammar Multani <ammarmultani007@gmail.com>
2025-01-15 18:18:25 +0530

    Final App build

--- App.tsx
+++ App.tsx
@@ -192,6 +192,7 @@ export default function App() {
                 ),
               }}
             />
+            
           </Drawer.Navigator>
         </NavigationContainer>
       </PersistGate>
